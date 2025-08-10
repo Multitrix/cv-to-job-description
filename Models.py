349100ce162dac74c6ac2cdca4a718f06b28ea12
amt278 <adhamtawfik@gmail.com>
2025-08-10 13:07:47 +0300

    Enhance API functionality: Implement personal info retrieval and saving, and add CV file retrieval endpoint

--- Models.py
+++ Models.py
@@ -1,6 +1,21 @@
 from pydantic import BaseModel
-
+from typing import Optional
 
 class CVRequest(BaseModel):
     personal_info: dict
     job_description: str
+
+class PersonalInfo(BaseModel):
+    name: str
+    email: str
+    address: Optional[str]
+    portfolio: Optional[str]
+    phone: str
+    linkedIn: str
+    github: Optional[str]
+    skills: list[str]
+    experience: Optional[list[dict]]
+    education: list[dict]
+    certifications: Optional[list[dict]]
+    languages: list[str]
+    projects: Optional[list[dict]]
--- api.py
+++ api.py
@@ -1,22 +1,36 @@
 from fastapi import FastAPI, HTTPException
-from Models import CVRequest
+from fastapi.responses import FileResponse
+import json
+import os
+from Models import CVRequest, PersonalInfo
 from cv_generator import save_cv
 
 app = FastAPI()
 
 
 @app.get('/personal-info')
-def get_personal_info():
-    pass
+def get_personal_info(file_path="generated\\personal_info.json"):
+    try:
+        with open(file_path, 'r') as file:
+            return json.load(file)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @app.post('/personal-info')
-def post_personal_info():
-    pass
+def post_personal_info(request: PersonalInfo):
+    try:
+        PersonalInfo = request.model_dump_json()
+        file_path = 'generated/personal_info.json'
+        with open(file_path, 'w') as file:
+            json.dump(PersonalInfo, file, indent=4)
+        return {'message': 'Personal info saved successfully', 'file_path': file_path}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @app.post('/job-description')
-def post_job_description():
+def post_job_description(description:str):
     pass
 
 
@@ -30,6 +44,11 @@ def generate_cv(request: CVRequest):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.get('/cv/{cv_id}')
-def get_cv(cv_id):
-    pass
+@app.get('/cv/{file_path}')
+def get_cv(file_path):
+    try:
+        if os.path.exists(file_path):
+            return FileResponse(path=file_path, media_type='application/pdf')
+        return {'error': 'File not found'}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
